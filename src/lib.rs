b0719e88144b5dbd6944dcde9927ff34b7a4fd5e
hazyfossa <hazyfossa@gmail.com>
2026-04-10 21:43:35 +0500

    Release

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,102 @@
+use std::net::SocketAddr;
 
+pub use eyre;
+pub use facet::Facet;
+pub use figue as args;
+
+use axum_client_ip::ClientIpSource;
+use eyre::{Context, Result, bail};
+use tokio::net::TcpListener;
+
+mod utils;
+use tower::ServiceBuilder;
+use tower_http::{catch_panic::CatchPanicLayer, compression::CompressionLayer};
+pub use utils::{errors, scheduler};
+
+use crate::utils::assets::ServeAssets;
+
+async fn setup_network(config: &BuiltInConfig) -> Result<(TcpListener, ClientIpSource)> {
+    let addr = &config.bind_address;
+    let listener = TcpListener::bind(addr)
+        .await
+        .context("Failed to bind listener")?;
+
+    tracing::info!("Listening on http://{addr}");
+
+    let ip_source = match &config.reverse_proxy {
+        None => ClientIpSource::ConnectInfo,
+        Some(proxy) => match proxy.as_str() {
+            "nginx" => ClientIpSource::XRealIp,
+            "cloudflare" => ClientIpSource::CfConnectingIp,
+            "cloudfront" => ClientIpSource::CloudFrontViewerAddress,
+            "flyio" => ClientIpSource::FlyClientIp,
+            // TODO: support full axum-client-ip
+            other => bail!("{other} is not a supported proxy type"),
+        },
+    };
+
+    Ok((listener, ip_source))
+}
+
+#[derive(Facet)]
+struct BuiltInConfig {
+    bind_address: String,
+    reverse_proxy: Option<String>,
+    #[facet(flatten)]
+    builtins: figue::FigueBuiltins,
+}
+
+#[derive(Facet)]
+struct WithServerConfig<T> {
+    #[facet(flatten)]
+    built_in: BuiltInConfig,
+    #[facet(flatten)]
+    user_defined: T,
+}
+
+pub async fn web_serve<Server: WebServer>() -> Result<()> {
+    simple_eyre::install()?;
+    tracing_subscriber::fmt::init();
+
+    let config = figue::builder::<WithServerConfig<Server>>()?
+        .cli(|c| c.args_os(std::env::args_os()))
+        .env(|c| c.prefix(Server::APPNAME.to_uppercase()))
+        .file(|c| c.default_paths(["./config.json"]))
+        // TODO: support version, description
+        .help(|c| c.program_name(Server::APPNAME))
+        .build();
+
+    // NOTE: this unwrap is preferred to Result
+    let config = figue::Driver::new(config).run().unwrap();
+
+    let (listener, ip_source) = setup_network(&config.built_in).await?;
+
+    let router = Server::init(config.user_defined).await?;
+
+    let router = router
+        .fallback_service(ServeAssets(Server::assets()))
+        .layer(
+            ServiceBuilder::new()
+                .layer(CatchPanicLayer::new())
+                .layer(CompressionLayer::new())
+                .layer(ip_source.into_extension()),
+        );
+
+    let service = router.into_make_service_with_connect_info::<SocketAddr>();
+
+    axum::serve(listener, service)
+        .with_graceful_shutdown(utils::shutdown::signal())
+        .await?;
+
+    Ok(())
+}
+
+#[allow(async_fn_in_trait)]
+pub trait WebServer: for<'a> Facet<'a> {
+    const APPNAME: &str;
+
+    // TODO: better asset handling
+    fn assets() -> &'static (impl rust_embed::Embed + Send + Sync);
+
+    async fn init(self) -> Result<axum::Router>;
+}
---
+++ src/utils.rs
@@ -0,0 +1,206 @@
+pub mod shutdown {
+    use tokio::signal;
+
+    pub async fn signal() {
+        let ctrl_c = async {
+            signal::ctrl_c()
+                .await
+                .expect("failed to install Ctrl+C handler");
+        };
+
+        #[cfg(unix)]
+        let terminate = async {
+            signal::unix::signal(signal::unix::SignalKind::terminate())
+                .expect("failed to install signal handler")
+                .recv()
+                .await;
+        };
+
+        #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+        tokio::select! {
+            _ = ctrl_c => {},
+            _ = terminate => {},
+        }
+
+        tracing::info!("Shutting down");
+    }
+}
+
+pub mod scheduler {
+    pub use time::Duration as Interval;
+
+    pub fn schedule_task<F, Fut>(name: &str, interval: Interval, task_fn: F)
+    where
+        F: Fn() -> Fut + Send + Sync + 'static,
+        Fut: Future<Output = ()> + Send,
+    {
+        tracing::info!("Scheduled {name} to run every {interval}");
+
+        let mut interval = tokio::time::interval(std::time::Duration::from_secs(
+            interval.whole_seconds() as u64,
+        ));
+
+        tokio::spawn(async move {
+            loop {
+                interval.tick().await;
+                task_fn().await;
+            }
+        });
+    }
+}
+
+#[allow(dead_code)]
+pub mod errors {
+    use axum::{BoxError, http::StatusCode, response::IntoResponse};
+
+    #[derive(Debug)]
+    enum WebErrorKind {
+        Client,
+        Internal,
+    }
+
+    #[derive(Debug)]
+    pub struct WebError {
+        kind: WebErrorKind,
+        inner: BoxError,
+        code: Option<StatusCode>,
+    }
+
+    impl WebError {
+        pub fn code(mut self, value: StatusCode) -> Self {
+            self.code.replace(value);
+            self
+        }
+
+        pub fn internal(error: BoxError) -> Self {
+            WebError {
+                kind: WebErrorKind::Internal,
+                inner: error,
+                code: None,
+            }
+        }
+
+        pub fn client(error: BoxError) -> Self {
+            WebError {
+                kind: WebErrorKind::Client,
+                inner: error,
+                code: None,
+            }
+        }
+    }
+
+    // TODO: color-eyre does not play well with tracing
+
+    impl IntoResponse for WebError {
+        fn into_response(self) -> axum::response::Response {
+            match self.kind {
+                WebErrorKind::Client => {
+                    tracing::warn!("Client error: {}", self.inner);
+                    (
+                        self.code.unwrap_or(StatusCode::BAD_REQUEST),
+                        self.inner.to_string(),
+                    )
+                }
+                WebErrorKind::Internal => {
+                    tracing::error!("Internal server error: {}", self.inner);
+                    (
+                        self.code.unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
+                        "Something went wrong".to_string(),
+                    )
+                }
+            }
+            .into_response()
+        }
+    }
+
+    pub type WebResult<T> = Result<T, WebError>;
+
+    impl From<(StatusCode, &'static str)> for WebError {
+        fn from(value: (StatusCode, &'static str)) -> Self {
+            let (code, string) = value;
+            Self::internal(string.into()).code(code)
+        }
+    }
+
+    // Eyre integration
+
+    impl From<eyre::Error> for WebError {
+        fn from(value: eyre::Error) -> Self {
+            Self::internal(value.into())
+        }
+    }
+
+    pub trait EyreWebExt {
+        fn client_error(self) -> WebError;
+    }
+
+    impl EyreWebExt for eyre::Error {
+        fn client_error(self) -> WebError {
+            WebError::client(self.into())
+        }
+    }
+}
+
+pub mod assets {
+    use axum::{
+        extract::Request,
+        http::{Method, StatusCode, header},
+        response::{IntoResponse, Response},
+    };
+    use eyre::eyre;
+    use rust_embed::RustEmbed;
+
+    use super::errors::{EyreWebExt, WebResult};
+
+    pub struct ServeAssets<T: 'static>(pub &'static T);
+
+    impl<T: 'static> Clone for ServeAssets<T> {
+        fn clone(&self) -> Self {
+            Self(self.0)
+        }
+    }
+
+    fn asset_lookup<A: RustEmbed>(request: Request) -> WebResult<Response> {
+        let uri = request.uri().path().trim_start_matches('/');
+
+        let content = A::get(uri).ok_or(
+            eyre!("404 Not Found")
+                .client_error()
+                .code(StatusCode::NOT_FOUND),
+        )?;
+
+        if request.method() != Method::GET {
+            return Err(eyre!("Method Not Allowed")
+                .client_error()
+                .code(StatusCode::METHOD_NOT_ALLOWED));
+        };
+
+        Ok((
+            [(header::CONTENT_TYPE, content.metadata.mimetype())],
+            content.data,
+        )
+            .into_response())
+    }
+
+    impl<A> tower::Service<Request> for ServeAssets<A>
+    where
+        A: RustEmbed,
+    {
+        type Error = std::convert::Infallible;
+        type Future = std::future::Ready<Result<Response, Self::Error>>;
+        type Response = Response;
+
+        fn call(&mut self, request: Request) -> Self::Future {
+            std::future::ready(Ok(asset_lookup::<A>(request).into_response()))
+        }
+
+        fn poll_ready(
+            &mut self,
+            _: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), Self::Error>> {
+            std::task::Poll::Ready(Ok(()))
+        }
+    }
+}
